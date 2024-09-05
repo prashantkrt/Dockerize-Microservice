3e95e4945dd806c64b5d37192a350521f7709d87
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-05 10:39:47 +0530

    Coupan Service

---
+++ Coupan Service/src/main/java/com/mylearning/coupanservice/coupanservice/CoupanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.coupanservice.coupanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupanServiceApplication.class, args);
+    }
+
+}
---
+++ Coupan Service/src/test/java/com/mylearning/coupanservice/coupanservice/CoupanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.coupanservice.coupanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
