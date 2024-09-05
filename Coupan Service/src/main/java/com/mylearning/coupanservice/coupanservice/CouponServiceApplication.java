db8b570bf6f19a032441b59545d426d355058201
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-05 10:42:33 +0530

    Initial Commit

--- Coupan Service/src/main/java/com/mylearning/coupanservice/coupanservice/CouponServiceApplication.java
+++ Coupan Service/src/main/java/com/mylearning/coupanservice/coupanservice/CouponServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoupanServiceApplication {
+public class CouponServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoupanServiceApplication.class, args);
+        SpringApplication.run(CouponServiceApplication.class, args);
     }
 
 }
